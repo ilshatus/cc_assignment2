e095c55cca3fab30ce8c0f73a58cb329a79b7588
the_art_of_war <xildar98@mail.ru>
2018-09-21 23:00:55 +0300

    keyword fixed

--- src/main/java/LexicalAnalyzer.java
+++ src/main/java/LexicalAnalyzer.java
@@ -11,7 +11,6 @@ public class LexicalAnalyzer {
     private void addBuilders() {
         builders = new Builder[11];
         builders[0] = new PlainIdentifierToken.Builder();
-
         builders[1] = new LogicalLiteralToken.Builder();
         builders[2] = new MultilineStringLiteralToken.Builder();
         builders[3] = new NumericLiteralToken.Builder();
@@ -24,40 +23,40 @@ public class LexicalAnalyzer {
         builders[10] = new ParenthesesToken.Builder();
     }
 
-    private void preprocess(){
+    private void preprocess() {
         StringBuilder result = new StringBuilder();
         System.out.println("HERE \n" + inputCode);
-        for(int i = 0;i<inputCode.length()-1;++i){
-            if(inputCode.charAt(i)=='/' && inputCode.charAt(i+1)=='*'){
+        for (int i = 0; i < inputCode.length() - 1; ++i) {
+            if (inputCode.charAt(i) == '/' && inputCode.charAt(i + 1) == '*') {
                 boolean flag = false;
                 int pos = i;
-                for(int j = i+2;j<inputCode.length();++j){
-                    if(inputCode.charAt(j)=='*' && inputCode.charAt(j+1)=='/'){
-                        pos = j+1;
+                for (int j = i + 2; j < inputCode.length(); ++j) {
+                    if (inputCode.charAt(j) == '*' && inputCode.charAt(j + 1) == '/') {
+                        pos = j + 1;
                         flag = true;
                         break;
                     }
                 }
 
-                if(!flag){
+                if (!flag) {
                     System.out.println("not found end of comment");
                 }
                 i = pos;
                 continue;
-            } else if(inputCode.charAt(i)=='/' && inputCode.charAt(i+1)=='/'){
+            } else if (inputCode.charAt(i) == '/' && inputCode.charAt(i + 1) == '/') {
                 boolean flag = false;
                 int pos = i;
-                for(int j = i+2;j<inputCode.length();++j){
-                    if(inputCode.charAt(j)=='\n'){
-                        pos = j+1;
+                for (int j = i + 2; j < inputCode.length(); ++j) {
+                    if (inputCode.charAt(j) == '\n') {
+                        pos = j + 1;
                         flag = true;
                         break;
                     }
                 }
 
-                if(!flag){
+                if (!flag) {
                     i = inputCode.length();
-                } else{
+                } else {
                     i = pos;
                 }
                 continue;
@@ -78,53 +77,53 @@ public class LexicalAnalyzer {
     Token getNextToken() {
         addBuilders();
 
-        while(top < inputCode.length()){
+        while (top < inputCode.length()) {
             char c = inputCode.charAt(top);
-            if(c==' ' || c =='\n'){
-      //          System.out.println("skip " + c);
+            if (c == ' ' || c == '\n') {
+                //          System.out.println("skip " + c);
                 top++;
-            } else{
+            } else {
                 break;
             }
         }
         Builder last = null;
         int pos = top;
-     //   System.out.println("start decode " + top);
-        for(int i=top;i<inputCode.length();++i){
+        //   System.out.println("start decode " + top);
+        for (int i = top; i < inputCode.length(); ++i) {
             char ch = inputCode.charAt(i);
-         //   System.out.println("next char [" + ch+"]");
+            //   System.out.println("next char [" + ch+"]");
             int total = 0;
-            for(int j=0;j<builders.length;++j){
-                if(builders[j] == null)
+            for (int j = 0; j < builders.length; ++j) {
+                if (builders[j] == null)
                     continue;
                 total++;
                 State x = builders[j].addNextChar(ch);
-            //    System.out.println(ch + " buil "+j+" got " + x);
-                if(x == State.MATCH){
-                //    System.out.println("full match " + j + " on "+i);
+                //    System.out.println(ch + " buil "+j+" got " + x);
+                if (x == State.MATCH) {
+                    //    System.out.println("full match " + j + " on "+i);
                     pos = i;
                     last = builders[j];
-                } else if(x == State.NOT_MATCH){
+                } else if (x == State.NOT_MATCH) {
                     builders[j] = null;
                     --total;
                 }
             }
-            if(total == 0)
+            if (total == 0)
                 break;
 
         }
 
-        if(last == null)
-         System.out.println("last null");
+        if (last == null)
+            System.out.println("last null");
 
-        if(last == null && top<inputCode.length()){
+        if (last == null && top < inputCode.length()) {
             System.out.println("couldnt identify");
         }
         top = pos + 1; // move top for next token
 
-        if(last == null)
+        if (last == null)
             return null;
-     //   System.out.println("HERE ");
+        //   System.out.println("HERE ");
         return last.build();
     }
 
