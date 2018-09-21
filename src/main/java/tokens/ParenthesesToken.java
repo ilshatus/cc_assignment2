2950ad01530ee1ab63499279cd244b8019855107
Ilshat <g.ilshat.98@mail.ru>
2018-09-21 21:20:31 +0300

    first commit

---
+++ src/main/java/tokens/ParenthesesToken.java
@@ -0,0 +1,38 @@
+package tokens;
+
+import tokens.builders.SimpleRegexBuilder;
+import tokens.enums.State;
+
+import java.util.regex.Pattern;
+
+public class ParenthesesToken extends Token {
+    private String lexeme;
+
+    private ParenthesesToken(String lexeme) {
+        this.lexeme = lexeme;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("T_parentheses(%s)", lexeme);
+    }
+
+
+    public static class Builder extends SimpleRegexBuilder {
+        private static final String regexp =
+                "^[(){}\\[\\]]$";
+
+        public Builder() {
+            super();
+            super.p = Pattern.compile(regexp);
+        }
+
+        @Override
+        public ParenthesesToken build() {
+            if (state.equals(State.MATCH)) {
+                return new ParenthesesToken(lexeme.toString());
+            }
+            return null;
+        }
+    }
+}
