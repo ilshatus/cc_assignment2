238d3c531fc30121e2dc2fac3125fedafacc9bad
Ilshat <g.ilshat.98@mail.ru>
2018-09-22 20:54:06 +0300

    Add test classes

---
+++ src/test/java/UnitTests.java
@@ -0,0 +1,18 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                TestBackQuoteIdentifierToken.class,
+                TestDelimeterToken.class,
+                TestKeywordToken.class,
+                TestLexicalAnalyzer.class,
+                TestLiteralTokens.class,
+                TestParenthesesToken.class,
+                TestPlainIdentifierToken.class
+        }
+)
+public class UnitTests {
+
+}
