f1aa8ce78032d4263882390905fe148096cf2224
the_art_of_war <xildar98@mail.ru>
2018-09-23 21:52:19 +0300

    test xml

--- src/test/java/TestXmlToken.java
+++ src/test/java/TestXmlToken.java
@@ -57,4 +57,70 @@ public class TestXmlToken {
         }
         Assert.assertFalse(state == State.MATCH);
     }
+
+    @Test
+    public void test5() {
+        Builder builder = new XmlToken.Builder();
+        String input = "<asda> <asdas> ";
+        State state = null;
+        for(char c:input.toCharArray()){
+            state = builder.addNextChar(c);
+            if(state == State.NOT_MATCH)
+                break;
+        }
+        Assert.assertFalse(state == State.MATCH);
+    }
+
+
+    @Test
+    public void test6() {
+        Builder builder = new XmlToken.Builder();
+        String input = "<a> </a>";
+        State state = null;
+        for(char c:input.toCharArray()){
+            state = builder.addNextChar(c);
+            if(state == State.NOT_MATCH)
+                break;
+        }
+        Assert.assertTrue(state == State.MATCH);
+    }
+
+    @Test
+    public void test7() {
+        Builder builder = new XmlToken.Builder();
+        String input = "<a> <b> TExt some asd values </b> </a>";
+        State state = null;
+        for(char c:input.toCharArray()){
+            state = builder.addNextChar(c);
+            if(state == State.NOT_MATCH)
+                break;
+        }
+        Assert.assertTrue(state == State.MATCH);
+    }
+
+    @Test
+    public void test8() {
+        Builder builder = new XmlToken.Builder();
+        String input = "<a> <b> TExt \n \n adn <sasdad> va 0.5 15 asd some asd values </b> </a>";
+        State state = null;
+        for(char c:input.toCharArray()){
+            state = builder.addNextChar(c);
+            if(state == State.NOT_MATCH)
+                break;
+        }
+        Assert.assertTrue(state == State.MATCH);
+    }
+
+    @Test
+    public void test9() {
+        Builder builder = new XmlToken.Builder();
+        String input = "";
+        State state = null;
+        for(char c:input.toCharArray()){
+            state = builder.addNextChar(c);
+            if(state == State.NOT_MATCH)
+                break;
+        }
+        Assert.assertFalse(state == State.MATCH);
+    }
 }
