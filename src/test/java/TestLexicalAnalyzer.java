7ac08ab46057871c1cf18b7adaf8031a1a6b2483
the_art_of_war <xildar98@mail.ru>
2018-09-23 20:03:07 +0300

    tests done

--- src/test/java/TestLexicalAnalyzer.java
+++ src/test/java/TestLexicalAnalyzer.java
@@ -55,4 +55,246 @@ public class TestLexicalAnalyzer {
         Assert.assertTrue(lexicalAnalyzer.getNextToken() == null);
         Assert.assertTrue(lexicalAnalyzer.getNextToken() == null);
     }
+
+    /*   @Test
+       public void test() {
+           String input = ;
+           LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(input);
+           Token token;
+           String answer = "";
+           while ((token = lexicalAnalyzer.getNextToken()) != null) {
+               answer = answer + token.toString() + "\n";
+           }
+           String res =
+           Assert.assertTrue(answer.equals( res));
+       }
+   */
+    @Test
+    public void test5() {
+        String input = "object HelloWorld {\n" +
+                "    def main(args: Array[String]) {\n" +
+                "      println(\"Hello, world!\")\n" +
+                "    }\n" +
+                "  }";
+        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(input);
+        Token token;
+        String answer = "";
+        while ((token = lexicalAnalyzer.getNextToken()) != null) {
+            answer = answer + token.toString() + "\n";
+        }
+        String res = "T_keyword(object)\n" +
+                "T_identifier(HelloWorld)\n" +
+                "T_parentheses({)\n" +
+                "T_delimiter(new_line)\n" +
+                "T_keyword(def)\n" +
+                "T_identifier(main)\n" +
+                "T_parentheses(()\n" +
+                "T_identifier(args)\n" +
+                "T_keyword(:)\n" +
+                "T_identifier(Array)\n" +
+                "T_parentheses([)\n" +
+                "T_identifier(String)\n" +
+                "T_parentheses(])\n" +
+                "T_parentheses())\n" +
+                "T_parentheses({)\n" +
+                "T_delimiter(new_line)\n" +
+                "T_identifier(println)\n" +
+                "T_parentheses(()\n" +
+                "T_literal_string(\"Hello, world!\")\n" +
+                "T_parentheses())\n" +
+                "T_delimiter(new_line)\n" +
+                "T_parentheses(})\n" +
+                "T_delimiter(new_line)\n" +
+                "T_parentheses(})\n" +
+                "T_end_of_file()\n";
+        Assert.assertTrue(answer.equals(res));
+    }
+
+    @Test
+    public void test6() {
+        String input = "object HelloWorld2 extends Application {\n" +
+                "  println(\"Hello, world!\")\n" +
+                "}";
+        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(input);
+        Token token;
+        String answer = "";
+        while ((token = lexicalAnalyzer.getNextToken()) != null) {
+            answer = answer + token.toString() + "\n";
+        }
+        String res = "T_keyword(object)\n" +
+                "T_identifier(HelloWorld2)\n" +
+                "T_keyword(extends)\n" +
+                "T_identifier(Application)\n" +
+                "T_parentheses({)\n" +
+                "T_delimiter(new_line)\n" +
+                "T_identifier(println)\n" +
+                "T_parentheses(()\n" +
+                "T_literal_string(\"Hello, world!\")\n" +
+                "T_parentheses())\n" +
+                "T_delimiter(new_line)\n" +
+                "T_parentheses(})\n" +
+                "T_end_of_file()\n";
+        Assert.assertTrue(answer.equals(res));
+    }
+
+
+       @Test
+       public void test7() {
+           String input = "object abstractTypes extends Application {\n" +
+                   "  abstract class Buffer {\n" +
+                   "    type T; val element: T\n" +
+                   "  }\n" +
+                   "  abstract class SeqBuffer {\n" +
+                   "    type T; val element: Seq[T]; def length = element.length\n" +
+                   "  }\n" +
+                   "  def newIntBuffer(el: Int) = new Buffer {\n" +
+                   "    type T = Int; val element = el\n" +
+                   "  }\n" +
+                   "  def newIntBuffer(el: Int*) = new SeqBuffer {\n" +
+                   "    type T = Int; val element = el\n" +
+                   "  }\n" +
+                   "  println(newIntBuffer(1).element)\n" +
+                   "  println(newIntBuffer(1, 2, 3).length)\n" +
+                   "}\n" +
+                   "Printer-friendly versionPrinter-friendly version\n" ;
+           LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(input);
+           Token token;
+           String answer = "";
+           while ((token = lexicalAnalyzer.getNextToken()) != null) {
+               answer = answer + token.toString() + "\n";
+           }
+           String res = "T_keyword(object)\n" +
+                   "T_identifier(abstractTypes)\n" +
+                   "T_keyword(extends)\n" +
+                   "T_identifier(Application)\n" +
+                   "T_parentheses({)\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_keyword(abstract)\n" +
+                   "T_keyword(class)\n" +
+                   "T_identifier(Buffer)\n" +
+                   "T_parentheses({)\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_keyword(type)\n" +
+                   "T_identifier(T)\n" +
+                   "T_delimiter(comma)\n" +
+                   "T_keyword(val)\n" +
+                   "T_identifier(element)\n" +
+                   "T_keyword(:)\n" +
+                   "T_identifier(T)\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_parentheses(})\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_keyword(abstract)\n" +
+                   "T_keyword(class)\n" +
+                   "T_identifier(SeqBuffer)\n" +
+                   "T_parentheses({)\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_keyword(type)\n" +
+                   "T_identifier(T)\n" +
+                   "T_delimiter(comma)\n" +
+                   "T_keyword(val)\n" +
+                   "T_identifier(element)\n" +
+                   "T_keyword(:)\n" +
+                   "T_identifier(Seq)\n" +
+                   "T_parentheses([)\n" +
+                   "T_identifier(T)\n" +
+                   "T_parentheses(])\n" +
+                   "T_delimiter(comma)\n" +
+                   "T_keyword(def)\n" +
+                   "T_identifier(length)\n" +
+                   "T_keyword(=)\n" +
+                   "T_identifier(element)\n" +
+                   "T_delimiter(dot)\n" +
+                   "T_identifier(length)\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_parentheses(})\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_keyword(def)\n" +
+                   "T_identifier(newIntBuffer)\n" +
+                   "T_parentheses(()\n" +
+                   "T_identifier(el)\n" +
+                   "T_keyword(:)\n" +
+                   "T_identifier(Int)\n" +
+                   "T_parentheses())\n" +
+                   "T_keyword(=)\n" +
+                   "T_keyword(new)\n" +
+                   "T_identifier(Buffer)\n" +
+                   "T_parentheses({)\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_keyword(type)\n" +
+                   "T_identifier(T)\n" +
+                   "T_keyword(=)\n" +
+                   "T_identifier(Int)\n" +
+                   "T_delimiter(comma)\n" +
+                   "T_keyword(val)\n" +
+                   "T_identifier(element)\n" +
+                   "T_keyword(=)\n" +
+                   "T_identifier(el)\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_parentheses(})\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_keyword(def)\n" +
+                   "T_identifier(newIntBuffer)\n" +
+                   "T_parentheses(()\n" +
+                   "T_identifier(el)\n" +
+                   "T_keyword(:)\n" +
+                   "T_identifier(Int)\n" +
+                   "T_identifier(*)\n" +
+                   "T_parentheses())\n" +
+                   "T_keyword(=)\n" +
+                   "T_keyword(new)\n" +
+                   "T_identifier(SeqBuffer)\n" +
+                   "T_parentheses({)\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_keyword(type)\n" +
+                   "T_identifier(T)\n" +
+                   "T_keyword(=)\n" +
+                   "T_identifier(Int)\n" +
+                   "T_delimiter(comma)\n" +
+                   "T_keyword(val)\n" +
+                   "T_identifier(element)\n" +
+                   "T_keyword(=)\n" +
+                   "T_identifier(el)\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_parentheses(})\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_identifier(println)\n" +
+                   "T_parentheses(()\n" +
+                   "T_identifier(newIntBuffer)\n" +
+                   "T_parentheses(()\n" +
+                   "T_literal_integer(1)\n" +
+                   "T_parentheses())\n" +
+                   "T_delimiter(dot)\n" +
+                   "T_identifier(element)\n" +
+                   "T_parentheses())\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_identifier(println)\n" +
+                   "T_parentheses(()\n" +
+                   "T_identifier(newIntBuffer)\n" +
+                   "T_parentheses(()\n" +
+                   "T_literal_integer(1)\n" +
+                   "T_delimiter(comma)\n" +
+                   "T_literal_integer(2)\n" +
+                   "T_delimiter(comma)\n" +
+                   "T_literal_integer(3)\n" +
+                   "T_parentheses())\n" +
+                   "T_delimiter(dot)\n" +
+                   "T_identifier(length)\n" +
+                   "T_parentheses())\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_parentheses(})\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_identifier(Printer)\n" +
+                   "T_identifier(-)\n" +
+                   "T_identifier(friendly)\n" +
+                   "T_identifier(versionPrinter)\n" +
+                   "T_identifier(-)\n" +
+                   "T_identifier(friendly)\n" +
+                   "T_identifier(version)\n" +
+                   "T_delimiter(new_line)\n" +
+                   "T_end_of_file()\n";
+           Assert.assertTrue(answer.equals( res));
+       }
+
+
 }
